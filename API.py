8888f58c3f151f316d317ee44e8e69b0c1b5f9a6
ekdnast <126035960+ekdnast@users.noreply.github.com>
2023-05-29 09:22:45 +0700

    eka#1
    makesure

--- API.py
+++ API.py
@@ -1,13 +1,13 @@
-Barang Jualan = {
+barang = {
      "Dress wanita" : 150000,
     "Celana Kulot"  : 850000,
     "Celana jeans"  : 210000,
     "Kemeja wanita" : 142000,
-    "blazer wanita" : 130000
+    "blazer wanita" : 130000,
     }
 Print("===================== Daftar Harga =====================") 
-for i in menu:
-    Print("Daftar Menu : ", i,"\t Harga : ",menu[i])
+for i in barang:
+    Print("Daftar Menu : ", i,"\t Harga : ",barang[i])
     Print("Pembelian diatas Rp100.000,- mendapatkan diskon 15%")
     Print("===================================================")
     Beli = Input ("Pilih Menu : ") 
